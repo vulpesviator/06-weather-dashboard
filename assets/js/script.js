55346c9103f415ac3c714688d55353760c28d843
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-05-08 17:25:36 -0500

    Forecast API hooked up

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,7 @@ var citySearch = $(".city-search");
 
 function getWeather(data) {
   var apiKey = "eac8b0ada86d323c106004da27e70b99";
-  // var lat = "41.948438";
-  // var lon = "-87.655333";
+  
   var cityName = citySearch.val().trim();;
   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
 
@@ -27,6 +26,10 @@ function getWeather(data) {
     })
     .then(function (data) {
       console.log(data);
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      console.log(lat);
+      console.log(lon);
 
       currentCity.innerHTML = data.name;
 
@@ -47,7 +50,24 @@ function getWeather(data) {
       currentHumidity.innerHTML = `${data.main.humidity}%`;
 
       currentDate.innerHTML = dayjs().format("dddd, MMMM D, YYYY");
+
+      getForecast();
     });
+}
+
+function getForecast() {
+  var apiKey = "eac8b0ada86d323c106004da27e70b99";
+  var lat = "42.3314";
+  var lon = "-83.0458";
+  var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  fetch(forecastUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    })
 }
 
 searchBtn.on("click", getWeather);
